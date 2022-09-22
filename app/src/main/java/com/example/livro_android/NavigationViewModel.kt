c51522c13e5d710b33a95667a32776f49f33bf27
Pablo <pablorodrigues@mobills.com.br>
2022-09-21 23:06:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/livro_android/NavigationViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.livro_android
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NavigationViewModel @Inject constructor() : ViewModel()
